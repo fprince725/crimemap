2c254a635074fdebcc13f2befd01e38f9fb5a4c8
Frank Prince <Frank.Prince@cox.net>
2018-01-24 00:06:28 +0000

    syntax errors

--- dbhelper.py
+++ dbhelper.py
@@ -53,14 +53,14 @@ class DBHelper:
 			with connection.cursor() as cursor:
 				cursor.execute(query)
 
-				named_crimes[]
+				named_crimes = []
 
 				for crime in cursor:
 					named_crime = {
 					'latitude': crime[0],
 					'longitude': crime[1],
-					'date': datetime.datetime.strftime(crime[2], "%Y-%m-%d")
-					'category': crime[3]
+					'date': datetime.datetime.strftime(crime[2], "%Y-%m-%d"),
+					'category': crime[3],
 					'description': crime[4]
 					}
 					named_crimes.append(named_crime)
